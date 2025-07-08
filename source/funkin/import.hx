1ca589b8a4dd936e694c5b12de06fd3cbd10f914
TheLagKing <dalucanogheaven12@gmail.com>
2025-07-09 07:08:46 +0800

    Update import.hx

--- source/funkin/import.hx
+++ source/funkin/import.hx
@@ -24,7 +24,7 @@ import extension.androidtools.Settings as AndroidSettings;
 import extension.androidtools.Tools as AndroidTools;
 import extension.androidtools.os.Build.VERSION as AndroidVersion;
 import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
-import extension.androidtools.hardware.BatteryManager; as AndroidBatteryManager;
+import extension.androidtools.hardware.BatteryManager;
 #end
 
 import openfl.utils.Assets;
