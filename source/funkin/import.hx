3653983368f22615bf8d94cc47152c684f3e8fa4
TheLagKing <dalucanogheaven12@gmail.com>
2025-07-08 19:56:22 +0800

    Update import.hx

--- source/funkin/import.hx
+++ source/funkin/import.hx
@@ -16,6 +16,15 @@ import funkin.mobile.*;
 import funkin.mobile.backend.*;
 import funkin.mobile.controls.*;
 import funkin.mobile.utils.*;
+ import android.content.Context as AndroidContext;
+ import android.widget.Toast as AndroidToast;
+ import android.os.Environment as AndroidEnvironment;
+ import android.Permissions as AndroidPermissions;
+ import android.Settings as AndroidSettings;
+ import android.Tools as AndroidTools;
+ import android.os.Build.VERSION as AndroidVersion;
+ import android.os.Build.VERSION_CODES as AndroidVersionCode;
+ import android.os.BatteryManager as AndroidBatteryManager;
 #end
 
 import openfl.utils.Assets;
