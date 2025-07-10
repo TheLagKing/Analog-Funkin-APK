fd21d5f837e31c8377ee7e7d681afe26afc30abd
TheLagKing <dalucanogheaven12@gmail.com>
2025-07-10 23:00:44 +0800

    Update global.hx

--- assets/data/global.hx
+++ assets/data/global.hx
@@ -1,3 +1,8 @@
 //
+import funkin.backend.assets.ModsFolder;
+
+function preStateSwitch()
+    if (ModsFolder.currentModFolder != 'Analog Funkin')
+        ModsFolder.switchMod('Analog Funkin');
 function update(elapsed:Float)
 	if (FlxG.keys.justPressed.F5) FlxG.resetState();
