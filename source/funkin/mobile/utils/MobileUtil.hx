722397021882de1f27d732c80deba37f3a75ca9c
TheLagKing <dalucanogheaven12@gmail.com>
2025-08-01 17:14:55 +0800

    Update MobileUtil.hx

--- source/funkin/mobile/utils/MobileUtil.hx
+++ source/funkin/mobile/utils/MobileUtil.hx
@@ -1,15 +1,5 @@
 package funkin.mobile.utils;
 
-#if mobile
-import extension.androidtools.content.Context as AndroidContext;
-import extension.androidtools.widget.Toast as AndroidToast;
-import extension.androidtools.os.Environment as AndroidEnvironment;
-import extension.androidtools.Permissions as AndroidPermissions;
-import extension.androidtools.Settings as AndroidSettings;
-import extension.androidtools.Tools as AndroidTools;
-import extension.androidtools.os.Build.VERSION as AndroidVersion;
-import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
-#end
 /**
  * A storage class for mobile.
  */
